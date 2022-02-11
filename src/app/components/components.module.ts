7f307a322c8c4745128e0d378a088eee22e8dc77
diegozwolf <diegozuluaga88@gmail.com>
2022-02-11 17:44:35 -0500

    remove side component stablish it on home

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from './material.module';
 
-import { PagesModule } from '../pages/pages.module';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { HijoComponent } from './hijo/hijo.component';
 
 
@@ -14,18 +12,17 @@ import { HijoComponent } from './hijo/hijo.component';
 @NgModule({
   declarations: [
     NavbarComponent,
-    HijoComponent,
-    SidenavComponent
+    HijoComponent
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+
   ],
   exports: [
     MaterialModule,
     NavbarComponent,
-    HijoComponent,
-    SidenavComponent
+    HijoComponent
   ]
 })
 export class ComponentsModule {}
