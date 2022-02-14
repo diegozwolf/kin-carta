cb22b9ffd0ab467893fe7ccc5d4764a00912a13f
diegozwolf <diegozuluaga88@gmail.com>
2022-02-14 14:05:51 -0500

    add styles to card section

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
 
+  cardText: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.";
+  cardBtnText: string = "Explore Similar Study";
   constructor() { }
 
   ngOnInit(): void {
