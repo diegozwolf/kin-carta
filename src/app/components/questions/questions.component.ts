2c1af2a56c3c54e696b82a60e09aea2e85c6cc84
diegozwolf <diegozuluaga88@gmail.com>
2022-02-14 12:40:00 -0500

    tree styles

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -1,5 +1,5 @@
 import {NestedTreeControl} from '@angular/cdk/tree';
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, ViewEncapsulation} from '@angular/core';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
 import { DOCUMENT } from '@angular/common';
 
@@ -28,11 +28,17 @@ const TREE_DATA: QuestionNode[] = [
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.scss']
+  styleUrls: ['./questions.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class QuestionsComponent {
 
   isOpen: boolean = false;
+  title1: string = "Pellentesque egestas a nisl et imperdiet";
+  title2: string = "Integer semper vehicula egestas.";
+  content1: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed feugiat lorem. Duis ac pharetra lacus, quis mattis felis.Nam bibendum mattis purus, at posuere nunc mattis ut. Proin commodo facilisis feugiat. Proin convallis massa sit amet pharetra tempus. Duis tincidunt tellus massa, nec tempor odio mattis et. Maecenas viverra orci et ipsum consequat pellentesque. Aenean ligula leo, suscipit nec pulvinar a, faucibus condimentum metus. Vestibulum ligula lectus  Nam pretium velit ac maximus imperdiet. Nam molestie accumsan aliquet. Quisque et quam vulputate, commodo elit ut, ultricies nunc. Vivamus est tortor, ultricies id sodales ac, eleifend vitae arcu.";
+  content2: string = "Ut mollis, ipsum condimentum porta imperdiet, lacus quam cursus leo, sit amet suscipit elit erat et elit. Nunc a purus pulvinar, tempus purus vestibulum, lobortis massa. Donec vitae est ex. Sed vulputate libero nec tellus dapibus pharetra. Mauris pretium mauris vel felis euismod, ut sodales tortor molestie. Cras at pharetra enim. Donec pulvinar, felis a convallis consectetur, metus mauris vehicula enim, sed viverra ante arcu ut felis. Phasellus in felis at odio imperdiet consequat scelerisque eu quam. Curabitur ac pretium massa. Etiam viverra vulputate eros in volutpat.";
+
 
   treeControl = new NestedTreeControl<QuestionNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<QuestionNode>();
@@ -44,9 +50,6 @@ export class QuestionsComponent {
 
   ngOnInit(): void {
   }
-  // toggleTree(){
-  //   this.isOpen = !this.isOpen;
-  // }
 
 }
 
